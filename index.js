04eff31f9aeb16317f9e7f42a81cc9e57ecc089d
Nikita Thomas <nikitathomas@Nikitas-MBP.WiFi Extender>
2024-01-08 20:30:48 -0500

    input questions to build README

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Questions Array
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Can you provide a brief description of your project:',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What steps does the user need to take to install the application?'
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What guidance would you provide to the user on how to use your application?',
+      },
+      {
+        type: 'input',
+        name: 'contibution',
+        message: 'Are there any other parties who contributed to this project?',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'What tests can be run to be sure the application is running correctly?',
+      }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
